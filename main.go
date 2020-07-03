3eabebcdacf6deb0c1059d7cc26d7e8d3a65ffcd
w-haibara <hwhaibarawataru@gmail.com>
2020-07-04 01:39:07 +0900

    ひな形をつくった

---
+++ main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+	"runtime"
+)
+
+func statusAPIHandler(w http.ResponseWriter, r *http.Request) {
+	status := map[string]interface{}{
+		"status": map[string]interface{}{
+			"arch": runtime.GOARCH,
+			"os":   runtime.GOOS,
+		},
+	}
+
+	j, err := json.Marshal(status)
+
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(j)
+}
+
+func consoleAPIHandler(w http.ResponseWriter, r *http.Request) {
+	w.Write(nil)
+}
+
+func main() {
+	fmt.Println("gelato")
+
+	/*
+	 * Web Server
+	 */
+	const appDir = "./cite/dist"
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir(appDir))))
+	http.Handle("/chat", http.StripPrefix("/chat", http.FileServer(http.Dir("."))))
+
+	/*
+	 * API Server
+	 */
+	http.HandleFunc("/api/status", statusAPIHandler)
+	http.HandleFunc("/api/console", consoleAPIHandler)
+
+	/*
+	 * WebSocket
+	 */
+	http.HandleFunc("/ws/chat", func(w http.ResponseWriter, r *http.Request) {
+		//serveWs(w, r)
+	})
+
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
